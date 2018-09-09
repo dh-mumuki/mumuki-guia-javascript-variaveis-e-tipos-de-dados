db6b0a611bc759e91c0efe16c84f1c23cd7a49b1
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-09 01:17:59 -0400

    Mumuki Export on 2018-09-09 01:17:59 -0400

---
+++ 00002_Declaração de variáveis/corollary.md
@@ -0,0 +1,6 @@
+Para que nosso código seja puro :sorrindo:, é uma boa prática declarar cada variável em uma linha diferente
+
+```javascript
+var idade
+var nome
+```
---
+++ 00002_Declaração de variáveis/test..js
@@ -0,0 +1,8 @@
+describe("", function() {
+  it("Declarou a variável idade", function() {
+    assert.notEqual(edad, "undefined");
+  });
+    it("Declarou a variável nome", function() {
+    assert.notEqual(peso, "undefined");
+  });
+})
