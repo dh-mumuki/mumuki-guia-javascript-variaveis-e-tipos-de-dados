ea58e6393b9ac2b2bff621103687f3465b74999c
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-09 01:44:18 -0400

    Mumuki Export on 2018-09-09 01:44:18 -0400

---
+++ 00011_Mais sobre os booleanos/test..js
@@ -0,0 +1,18 @@
+describe("Descripción del grupo", function() {
+  it("Declarada e atribuída a variável umNumeroPequeno", function() {
+    assert.notEqual(umNumeroPequeno, "undefined");
+    assert.ok(typeof umNumeroPequeno === "number");
+  });
+    it("Variável declarada e atribuída umNumeroGrande", function() {
+    assert.notEqual(umNumeroGrande, "undefined");
+    assert.ok(typeof unNumeroGrande === "number");
+  });
+    it("Declarada e atribuída a variável eMenor", function() {
+    assert.notEqual(eMenor, "undefined");
+    assert.ok(eMenor === umNumeroPequeno < unNumeroGrande);
+  });
+    it("Declarada y asignada la variable esMayor", function() {
+    assert.notEqual(esMayor, "undefined");
+    assert.ok(esMayor === umNumeroPequeno > unNumeroGrande);
+  });
+})
