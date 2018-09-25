2a6f6c351c47417e267fd416f6bc33d04031a339
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-25 18:23:31 -0400

    Mumuki Export on 2018-09-25 18:23:31 -0400

--- 00002_Declaração de variáveis/test..js
+++ 00002_Declaração de variáveis/test..js
@@ -1,6 +1,6 @@
 describe("", function() {
   it("Declarou a variável idade", function() {
-    assert.notEqual(edad, "undefined");
+    assert.notEqual(idade, "undefined");
   });
     it("Declarou a variável nome", function() {
     assert.notEqual(peso, "undefined");
