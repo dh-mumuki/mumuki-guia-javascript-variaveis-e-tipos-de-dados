a4e783abd67e180e302138a607df120d74541131
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-26 10:35:40 -0400

    Mumuki Export on 2018-09-26 10:35:40 -0400

--- 00006_Outro tipo de dados/test..js
+++ 00006_Outro tipo de dados/test..js
@@ -1,5 +1,5 @@
-describe("Descripción del grupo", function() {
-  it("Declarada e asignada a variável saudacao", function() {
+describe("", function() {
+  it("Declarada e assinada a variável saudacao", function() {
     assert.notEqual(saudacao, "undefined");
     assert.ok(saudacao === "Hello World");
   })
