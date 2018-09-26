a145cc5596e61edab79e40f2c28595cfda5e17d2
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-26 10:28:28 -0400

    Mumuki Export on 2018-09-26 10:28:28 -0400

--- 00005_Variáveis e Operações/test..js
+++ 00005_Variáveis e Operações/test..js
@@ -1,28 +1,28 @@
 describe("", function() {
-  it("Declarada e asignada a variável numeroA", function() {
+  it("Declarada e assinada a variável numeroA", function() {
     assert.notEqual(numeroA, "undefined");
     assert.ok(typeof numeroA === "number");
   });
-  it("Declarada e asignada a variável numeroB", function() {
+  it("Declarada e assinada a variável numeroB", function() {
     assert.notEqual(numeroB, "undefined");
     assert.ok(typeof numeroB == "number");
   });
-  it("Declarada e asignada a variável resultadoSoma", function() {
+  it("Declarada e assinada a variável resultadoSoma", function() {
     assert.notEqual(resultadoSoma, "undefined");
     assert.ok(typeof resultadoSoma == "number");
     assert.ok(resultadoSoma === (numeroA+numeroB));
   });
-  it("Declarada e asignada a variável resultadoSubtracao", function() {
+  it("Declarada e assinada a variável resultadoSubtracao", function() {
     assert.notEqual(resultadoSubtracao, "undefined");
     assert.ok(typeof resultadoSubtracao == "number");
     assert.ok(resultadoSubtracao === (numeroA-numeroB));
   });
-  it("Declarada e asignada a variável resultadoMultiplicacao", function() {
+  it("Declarada e assinada a variável resultadoMultiplicacao", function() {
     assert.notEqual(resultadoMultiplicacao, "undefined");
     assert.ok(typeof resultadoMultiplicacao == "number");
     assert.ok(resultadoMultiplicacao === (numeroA*numeroB));
   });
-  it("Declarada e asignada a variável resultadoDivisao", function() {
+  it("Declarada e assinada a variável resultadoDivisao", function() {
     assert.notEqual(resultadoDivisao, "undefined");
     assert.ok(typeof resultadoDivisao == "number");
     assert.ok(resultadoDivisao === (numeroA/numeroB));
