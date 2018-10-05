a903e828d0fb3e3dc5870965357efa7ca57cd961
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-05 11:32:46 -0400

    Mumuki Export on 2018-10-05 11:32:46 -0400

--- 00007_Mais operações/test..js
+++ 00007_Mais operações/test..js
@@ -1,13 +1,13 @@
 describe("", function() {
-  it("Declarada e assinada a variável nome", function() {
+  it("Declarada e atribuída a variável nome", function() {
     assert.notEqual(nome, "undefined");
     assert.ok(typeof nome === "string");
   });
-  it("Declarada e assinada a variável sobrenome", function() {
+  it("Declarada e atribuída a variável sobrenome", function() {
     assert.notEqual(sobrenome, "undefined");
     assert.ok(typeof sobrenome == "string");
   });
-  it("Declarada e assinada a variável nomeCompleto", function() {
+  it("Declarada e atribuída a variável nomeCompleto", function() {
     assert.notEqual(nomeCompleto, "undefined");
     assert.ok(typeof nomeCompleto == "string");
     assert.ok(nomeCompleto === nome + " " + sobrenome)
