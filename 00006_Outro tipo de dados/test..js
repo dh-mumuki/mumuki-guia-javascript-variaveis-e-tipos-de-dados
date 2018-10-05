69ad503c223462f33d0ea56a597a4d6dcdad9ba9
andres@digitalhouse.com <andres@digitalhouse.com>
2018-10-05 11:31:46 -0400

    Mumuki Export on 2018-10-05 11:31:46 -0400

--- 00006_Outro tipo de dados/test..js
+++ 00006_Outro tipo de dados/test..js
@@ -1,6 +1,6 @@
 describe("", function() {
   it("Declarada e atribuida a variável saudacao", function() {
     assert.notEqual(saudacao, "undefined");
-    //assert.ok(saudacao === "Hello World");
+    assert.ok(saudacao === "Hello World");
   })
 })
